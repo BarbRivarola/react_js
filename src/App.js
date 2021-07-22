32b335fea833a51dc5bb50211172787df5e85305
BarbRivarola <barb.rivarola@gmail.com>
2021-07-22 18:55:09 -0300

    creacion de app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>San Francisco</h1>
+        <h2>Buzos y crop tops</h2>
+        <span>Traditional tattoo style</span>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
