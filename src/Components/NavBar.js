271b469c2b296d2a15deb9275161b03d61aec7e4
BarbRivarola <barb.rivarola@gmail.com>
2021-07-29 20:23:17 -0300

    navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,24 @@
+function App () {
+    return <>
+    <NavBar>
+        <nav>
+            <span>San Francisco</span>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Productos
+                </li>
+                <li>
+                    Preguntas frecuentes
+                </li>
+                <li>
+                    Contacto
+                </li>
+            </ul>
+        </nav>
+    </NavBar> 
+
+    </>   
+}
